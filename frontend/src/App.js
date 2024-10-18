6a2cc21363d3162d4360a3aa5907c1fe92dba18e
AndresJaime <utfortnitedestruction@gmail.com>
2024-10-18 06:41:08 -0300

    ruta agregada

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react';//
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import BarraLateral from './components/BarraLateral';
 import Dashboard from './components/Dashboard';
