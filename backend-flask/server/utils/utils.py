3c3c02a40b6b2d54ddbd1d1c28fba254e4aa5c5f
MelanieSeg <smelanieseg@gmail.com>
2024-11-29 01:06:16 -0300

    Eliminar backend flask

--- backend-flask/server/utils/utils.py
+++
@@ -1,7 +0,0 @@
-from urllib.parse import urlparse, urlunparse
-
-
-def get_base_url(url):
-    parsed_url = urlparse(url)
-    base_url = urlunparse((parsed_url.scheme,parsed_url.netloc,'','','',''))
-    return base_url
